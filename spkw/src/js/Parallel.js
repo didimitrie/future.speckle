3e33afcbfc30ad0d6b7763f03121e9bb72a8d1e1
dimitrie <Bender@Benders-MacBook-Pro.local>
2016-03-22 15:14:55 +0000

    woot a mess

--- spkw/src/js/Parallel.js
+++ spkw/src/js/Parallel.js
@@ -1,178 +1,34 @@
 
-var $                   = require('jquery')
+var $                   = require('jquery'); window.$ = $;
 var SPK                 = require('./modules/SPK.js')
 var SPKMeta             = require('./modules/SPKMetaDisplay.js')
 var SPKParallelControl  = require('./modules/SPKParallel.js')
-var SPKCommentsControl  = require('./modules/SPKCommentsControl.js')
-var SPKHelpControl      = require('./modules/SPKHelpControl.js')
 var SPKKeyHandler       = require('./modules/SPKKeyHandler.js')
-var d3                  = require('d3'); window.d3 = d3;
+var d3                  = require('./modules/external/d3.v3.min.js'); window.d3 = d3;
 var parcoords           = require('./modules/external/d3.parcoords.js');
 
 
 $( function () {
 
-  // d3 funcs and vars
+  var dod3 = function ( datax ) {
 
-var margin = {top: 50, right: 50, bottom: 50, left: 50},
-    width  = $("#spk-parameters").innerWidth() - margin.left - margin.right,
-    height = $("#spk-parameters").innerHeight() - margin.top - margin.bottom;
+  var myData = d3.csv.parse(datax);
 
-var x = d3.scale.ordinal().rangePoints([0, width], 1),
-    y = {},
-    dragging = {};
+  var pc = d3.parcoords()("#spk-parameters")
+    .data(myData)
+    .hideAxis(["name"])
+    .alpha(0.15)
+    .margin({ top: 15, left: 15, bottom: 15, right: 15 })
+    .mode("queue")
+    .render()
+    .brushMode("1D-axes");
 
-var line = d3.svg.line(),
-    axis = d3.svg.axis().orient("left"),
-    background,
-    foreground;
-
-var svg = d3.select("#spk-parameters").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")"+ "rotate(270, "+width/2 + "," + height/2 + ")")
-
-
-  function position(d) {
-    var v = dragging[d];
-    return v == null ? x(d) : v;
-  }
-
-  function transition(g) {
-    return g.transition().duration(500);
-  }
-
-  // Returns the path for a given data point.
-  function path(d) {
-    return line(dimensions.map(function(p) { return [position(p), y[p](d[p])]; }));
-  }
-
-  function brushstart() {
-    d3.event.sourceEvent.stopPropagation();
-  }
-
-  // Handles a brush event, toggling the display of foreground lines.
-  function brush() {
-    var actives = dimensions.filter(function(p) { return !y[p].brush.empty(); }),
-        extents = actives.map(function(p) { return y[p].brush.extent(); });
-    foreground.style("display", function(d) {
-      return actives.every(function(p, i) {
-        return extents[i][0] <= d[p] && d[p] <= extents[i][1];
-      }) ? null : "none";
-    });
-  }
-
-  var dod3 = function ( data ) {
-    var cars = d3.csv.parse( data );
-
-    console.log(cars);
-    var colors = d3.scale.category20b(); 
-
-    var colorgen = d3.scale.ordinal()
-    .range(["#a6cee3","#1f78b4","#b2df8a","#33a02c",
-            "#fb9a99","#e31a1c","#fdbf6f","#ff7f00",
-            "#cab2d6","#6a3d9a","#ffff99","#b15928"]);
-    var color = function(d) { return colors(d.group); };
-
-    var pc = d3.parcoords()("#spk-parameters")
-      .data(cars)
-      .hideAxis(["name"])
-      .color(color)
-      .alpha(0.15)
-      .composite("darken")
-      .margin({ top: 24, left: 20, bottom: 12, right: 0 })
-      .mode("queue")
-      //.bundlingStrength(0.2)
-      //.smoothness(0.3)
-      .render()
-      .brushMode("1D-axes");  // enable brushing
-   
-   return;
-    // Extract the list of dimensions and create a scale for each.
-    x.domain(dimensions = d3.keys(cars[0]).filter(function(d) {
-      console.log(d);
-      return d != "name" && (y[d] = d3.scale.linear()
-          .domain(d3.extent(cars, function(p) { return +p[d]; }))
-          .range([height, 0]));
-    }));
-
-    // Add grey background lines for context.
-    background = svg.append("g")
-      .attr("class", "background")
-      .selectAll("path")
-      .data(cars)
-      .enter().append("path")
-      .attr("d", path);
-
-    // Add blue foreground lines for focus.
-    foreground = svg.append("g")
-      .attr("class", "foreground")
-      .selectAll("path")
-      .data(cars)
-      .enter().append("path")
-      .attr("d", path);
-
-    // Add a group element for each dimension.
-    var g = svg.selectAll(".dimension")
-      .data(dimensions)
-      .enter().append("g")
-        .attr("class", "dimension")
-        .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
-        .call(d3.behavior.drag()
-          .origin(function(d) { return {x: x(d)}; })
-          .on("dragstart", function(d) {
-            dragging[d] = x(d);
-            background.attr("visibility", "hidden");
-          })
-          .on("drag", function(d) {
-            dragging[d] = Math.min(width, Math.max(0, d3.event.x));
-            foreground.attr("d", path);
-            dimensions.sort(function(a, b) { return position(a) - position(b); });
-            x.domain(dimensions);
-            g.attr("transform", function(d) { return "translate(" + position(d) + ")"; })
-          })
-          .on("dragend", function(d) {
-            delete dragging[d];
-            transition(d3.select(this)).attr("transform", "translate(" + x(d) + ")");
-            transition(foreground).attr("d", path);
-            background
-              .attr("d", path)
-              .transition()
-                .delay(500)
-                .duration(0)
-                .attr("visibility", null);
-          }));
-
-    // Add an axis and title.
-    g.append("g")
-        .attr("class", "axis")
-        .each(function(d) { d3.select(this).call(axis.scale(y[d])); })
-      .append("text")
-        .style("text-anchor", "middle")
-        .attr("y", -9)
-        .text(function(d) { return d; });
-
-    // Add and store a brush for each axis.
-    g.append("g")
-        .attr("class", "brush")
-        .each(function(d) {
-          d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brushstart", brushstart).on("brush", brush));
-        })
-      .selectAll("rect")
-        .attr("x", -8)
-        .attr("width", 16);
   }
 
   var mySPK  = new SPK( 
   {
     canvasid : 'spk-canvas', 
     onInitEnd : function ( SPK ) { 
-      
-      var myMeta = new SPKMeta ( {
-        wrapperid : 'spk-metadata',
-        spk : SPK.META
-      } );
 
       var mySliderCtrl = new SPKParallelControl ( { 
         wrapperid : 'spk-parameters',
@@ -182,17 +38,6 @@ var svg = d3.select("#spk-parameters").append("svg")
         spk : SPK,
         onInitEnd : dod3
       } );
-
-      var myHelpCtrl = new SPKHelpControl ( {
-        wrapperid : 'spk-help',
-        uitabid : 'spk-ui-tabs',
-        icon : 'fa-info-circle'
-        //icon : 'fa-cogs'
-      })
-
-      var myKeyHandler = new SPKKeyHandler ( {
-        spk: SPK
-      })
       //window.SPK = mySPK;
     }
   } )
